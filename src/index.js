88d33b8740403a13ab0aa80ae8454744e1f3638d
Pablo Martinez <pablodmv@gmail.com>
2019-08-16 17:22:10 -0300

    structure

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+//Settings
+
+
+
+// Middlewares
+
+
+//Routes
+
+
+
+//Static files
+
+
+//Start Server
+
+app.listen(3000, ()=>{
+
+    console.log("Server on port 3000");
+    
+});
